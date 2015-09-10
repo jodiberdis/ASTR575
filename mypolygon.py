c0bceb6482d6470a2555cc2a277fc3ad51a75d03
Jodi Berdis <berdis@nmsu.edu>
2015-09-10 17:54:19 -0600

    Tring to rearrange files

---
+++ mypolygon.py
@@ -0,0 +1,25 @@
+from swampy.TurtleWorld import *
+
+world = TurtleWorld()
+bob = Turtle()
+print bob
+
+#fd(bob, 100)
+#lt(bob)
+#fd(bob, 100)
+#lt(bob)
+#fd(bob, 100)
+#lt(bob)
+#fd(bob, 100)
+
+for i in range(4):
+    fd(bob, 100)
+    lt(bob)
+
+#for i in range(4):
+#    print 'Hello!'
+
+def square(t):
+    
+
+wait_for_user()
