f65f9358645e305929df020515d26174f31a4140
Jodi Berdis <berdis@nmsu.edu>
2015-11-30 13:40:22 -0700

    Completed q29

--- q29/q29.py
+++ q29/q29.py
@@ -9,42 +9,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pylab import *
 
-#def func(x):
-#    fx = (x**-2.35)
-#    return fx
-
-
-def anintegral(a, b):          # Computes analytic integral
+def anintegral(a, b):
+    ''' Computes analytic integral of the Salpeter IMF Function '''
     return ((b**-1.35)/-1.35) - ((a**-1.35)/-1.35)
 
 masses = np.arange(0.5,100.5,0.5)     # Creates range of masses 0.5-100, with stepsize 0.5
 cumeinteg = np.zeros(len(masses))     # Empty array for cumulative integral values
 
+# Calculate the cumulative integral at each mass
 for i in range(len(masses)):
     cumeinteg[i] = anintegral(0.5,masses[i]) / anintegral(0.5,100.) # Divide resulting number by anintegral(0.5,100.) 
-#    print masses[i], cumeinteg[i]                                   #     to normalize so numbers go to 1 rather than 1.88
-
-
-# create array of random numbers between 0 and 1
-num = 1000
-randnum = np.random.random_sample((num))
-cumemass = np.zeros(len(randnum))
-
-# find corresponding x values (masses) - use where statement: where difference between random and actual equals minimum possible random - actual
-for i in range(len(randnum)):
-    index_cumemass = np.where(abs(randnum[i]-cumeinteg) == min(abs(randnum[i]-cumeinteg)))
-    cumemass[i] = masses[index_cumemass]
-print cumemass
-# Do histogram of resulting mass values 
-
-
+                                                                    #     to normalize so numbers go to 1 rather than 1.88
+# Set up a plot to populate 4 subplots
 plt.clf()
 plt.cla()
-bins = 99.5 / 0.5
-plt.hist(cumemass, bins)
+fig = plt.figure(figsize=(15,8))
+fig.subplots_adjust(wspace=0.4,hspace=0.4)
+bins = 99.5 / 0.5          # Number of bins for bin size=0.5
 
-plt.xlim(0,30)
-#plt.ylim(0,1)
-plt.xlabel('Mass')
-plt.ylabel('Number of Stars')
+# Create array of random numbers between 0 and 1
+num = [100,1000,5000,10000]   # Generate plots with several different sample sizes
+maxrange = [10,15,15,20]   # Maximum x axis range for plots
+for j in range(len(num)):
+    randnum = np.random.random_sample((num[j]))
+    cumemass = np.zeros(len(randnum))
+
+# Find corresponding x values (masses) - use where statement: where difference between random and actual equals minimum possible random - actual
+    for i in range(len(randnum)):
+        index_cumemass = np.where(abs(randnum[i]-cumeinteg) == min(abs(randnum[i]-cumeinteg)))
+        cumemass[i] = masses[index_cumemass]
+
+# Plot histogram of resulting mass value
+    ax=fig.add_subplot(2,2,j+1)
+    plt.hist(cumemass, bins)
+    plt.plot(masses[1:], (masses[1:]**-2.35)*(num[j]/anintegral(0.5,100.)))
+    plt.xlim(0,maxrange[j])
+    plt.xlabel('Mass')
+    plt.ylabel('Number of Stars')
+
 plt.show()
